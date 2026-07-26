0021f3546322e34cd7d80061b79dc5335a742737
Mark-Jhino <markjhinonoquera@gmail.com>
2026-07-26 16:02:53 +0800

    fixed?????

--- School Attendance System/School Attendance System/obj/Debug/net10.0-windows/School Attendance System.AssemblyInfo.cs
+++ School Attendance System/School Attendance System/obj/Debug/net10.0-windows/School Attendance System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("School Attendance System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d36429eff15fb74705fb9bd37ccd96d755a55e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+123587521c860a999b516ca278fd53412f9e7cde")]
 [assembly: System.Reflection.AssemblyProductAttribute("School Attendance System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("School Attendance System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
