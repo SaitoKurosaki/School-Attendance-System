d5a27ae635c84fd1b9027bc59a5dd7c5e31de9bf
Manuel Porcincula <manuellucioporcinculaxworld@gmail.com>
2026-08-13 13:48:39 +0800

    Add password hashing

--- School Attendance System/School Attendance System/obj/Debug/net10.0-windows/School Attendance System.AssemblyInfo.cs
+++ School Attendance System/School Attendance System/obj/Debug/net10.0-windows/School Attendance System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("School Attendance System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2534a3d476c69b049c03a01db37352659d01e9d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c378bd8891934df96639c19873465277617aecb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("School Attendance System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("School Attendance System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
